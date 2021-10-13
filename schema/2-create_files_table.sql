529f76b7d8a86861bdaf745e40a147f2ab0ec197
Richard Bradshaw <merryidleness@gmail.com>
2021-10-13 16:19:14 +0100

    Remove now unused file.

--- schema/2-create_files_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE files (
-       id INTEGER PRIMARY KEY,
-       file_path TEXT NOT NULL,
-       hash TEXT NOT NULL,
-       manifest_id INTEGER NOT NULL,
-       FOREIGN KEY (manifest_id) REFERENCES manifest (id)
-);
