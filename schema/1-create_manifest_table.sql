fadb784972101dd5822cf7875356e47b248cfab8
Richard Bradshaw <merryidleness@gmail.com>
2021-10-14 09:11:23 +0100

    Add a directory_path to the manifest.

--- schema/1-create_manifest_table.sql
+++ schema/1-create_manifest_table.sql
@@ -1,4 +1,5 @@
 CREATE TABLE manifest (
-       id INTEGER PRIMARY KEY,
-       timestamp INTEGER NOT NULL UNIQUE
+	id INTEGER PRIMARY KEY,
+	timestamp INTEGER NOT NULL UNIQUE,
+	directory_path TEXT NOT NULL
 );
